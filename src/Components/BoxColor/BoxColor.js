8a616d82f00800a5c77e47fbfef6abdf537e9768
mcapurri <m.capurri@gmail.com>
2021-02-23 09:42:07 +0100

    work in progress

--- src/Components/BoxColor/BoxColor.js
+++ src/Components/BoxColor/BoxColor.js
@@ -9,6 +9,7 @@ const BoxColor = (props) => {
         height: '200px',
         border: '1px solid #000',
         display: 'flex',
+        flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
       }}
@@ -16,8 +17,17 @@ const BoxColor = (props) => {
       <h1>
         rgb({props.r},{props.g},{props.b})
       </h1>
+      <h1>
+        #{componentToHex(props.r)}
+        {componentToHex(props.g)}
+        {componentToHex(props.b)}
+      </h1>
     </div>
   );
 };
 
+const componentToHex = (c) => {
+  let hex = c.toString(16);
+  return hex.length === 1 ? '0' + hex : hex;
+};
 export default BoxColor;
